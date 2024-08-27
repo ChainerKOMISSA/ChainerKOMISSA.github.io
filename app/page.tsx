d7ae95365e8d3eb50ebe357d39e857f0e649db82
Essi Chainer KOMISSA ZOTSU <essikomissa@gmail.com>
2024-08-28 01:00:54 +0200

    Chargement des nouveaux fichier

---
+++ app/page.tsx
@@ -0,0 +1,18 @@
+import Hero from "@/components/Hero";
+import { FloatingNavbar } from "@/components/ui/FloatingNavbar";
+import Image from "next/image";
+import { FaHome } from "react-icons/fa";
+
+export default function Home() {
+  return (
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <FloatingNavbar
+        navItems={[
+          {name : 'Accueil', link : './', icon : <FaHome/>}
+        ]}/>
+        <Hero/>
+      </div>
+    </main>
+  );
+}
